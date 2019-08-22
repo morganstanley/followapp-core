7bd587ff4ddd92d4ee71271e682bef4c357cb206
kartikpai <kartikpai@users.noreply.github.com>
2019-08-22 16:47:56 +0000

    updated create table ddl to v2 tables

--- src/main/sql/ddl-create-tables.sql
+++ src/main/sql/ddl-create-tables.sql
@@ -1,19 +1,33 @@
 use followapp;
 
 drop table if exists call_history;
-drop table if exists vaccination_details;
+drop table if exists prescription_reference;
 drop table if exists care_recipient;
-drop table if exists vaccination_description;
+drop table if exists prescription_details;
 
-CREATE TABLE vaccination_description (
-	vaccination_description_id 	int 		NOT NULL AUTO_INCREMENT,
-	disease 					varchar(50) NOT NULL,
-	dose 						int 		NOT NULL,
+CREATE TABLE prescription_reference (
+	prescription_reference_id 	int 		NOT NULL AUTO_INCREMENT,
+	prescription_name 					varchar(50) NOT NULL,
 	pre_req 					int 		NULL,
 	days_from_prev 				int 		NOT NULL,
-	PRIMARY KEY (vaccination_description_id),
-	FOREIGN KEY (pre_req) REFERENCES vaccination_description (vaccination_description_id)
+    is_recurring                bit         NOT NULL,
+    recurring_cron_schedule     varchar(50) NULL,
+    recurrence_instance_count   int         NULL;
+    call_config_id              int         NOT NULL;
+	PRIMARY KEY (prescription_reference_id),
+	FOREIGN KEY (pre_req) REFERENCES prescription_reference (prescription_reference_id),
+    FOREIGN KEY (call_config_id) REFERENCES prescription_reference (call_config)
 );
+
+CREATE TABLE call_config (
+	call_config 	int 		NOT NULL AUTO_INCREMENT,
+	call_type 					varchar(50) NOT NULL,
+	call_flow_id 				varchar(50) NOT NULL,
+	gap_btwn_retires 			int 		NULL,
+	max_number_of_retires 		int 		NULL,
+    capture_feedback            bit         NOT NULL;
+	PRIMARY KEY (call_config_id)
+    );
 
 CREATE TABLE care_recipient (
 	care_recipient_id 			int 			NOT NULL AUTO_INCREMENT,
@@ -39,28 +53,30 @@ CREATE TABLE care_recipient_extn (
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE vaccination_details (
-	vaccination_details_id 		int 		NOT NULL AUTO_INCREMENT,
+CREATE TABLE prescription_details (
+	prescription_details_id 		int 		NOT NULL AUTO_INCREMENT,
 	care_recipient_id 			int 		NOT NULL,
-	vaccination_description_id 	int 		NOT NULL,
+	prescription_reference_id 	int 		NOT NULL,
 	planned_date 				date 		DEFAULT NULL,
-	vaccination_status 			varchar(50) DEFAULT NULL,
-	PRIMARY KEY (vaccination_details_id),
+	call_status 	            varchar(50) NOT NULL,
+	PRIMARY KEY (prescription_details_id),
 	FOREIGN KEY (care_recipient_id) REFERENCES care_recipient (care_recipient_id) ,
-	FOREIGN KEY (vaccination_description_id) REFERENCES vaccination_description (vaccination_description_id),
-	UNIQUE KEY (care_recipient_id, vaccination_description_id)
-);
+	FOREIGN KEY (prescription_reference_id) REFERENCES prescription_reference (prescription_reference_id),
+	UNIQUE KEY (care_recipient_id, prescription_reference_id)
+)
 
 CREATE TABLE call_history (
-	call_track_id 				int 		NOT NULL AUTO_INCREMENT,
+	call_history_id 				int 		NOT NULL AUTO_INCREMENT,
 	ivr_call_id 				varchar(200)	NOT NULL UNIQUE,
 	care_recipient_id 			int 		NOT NULL,
-	vaccination_details_id 		int 		NOT NULL,  
+	prescription_details_id 	int 		NOT NULL,  
 	date_time_called 			datetime 	NOT NULL,  
 	call_status 				varchar(50) NOT NULL,
-	PRIMARY KEY (call_track_id),
+    call_duration               int         NOT NULL;
+	PRIMARY KEY (call_history_id),
 	FOREIGN KEY (care_recipient_id) REFERENCES care_recipient (care_recipient_id),
-	FOREIGN KEY (vaccination_details_id) REFERENCES vaccination_details(vaccination_details_id)
+	FOREIGN KEY (prescription_details_id) REFERENCES prescription_details(prescription_details_id)
 );
 
-show tables;
+
+
