b9a336bb332f895a3a6fd60a7952f905a1681f50
Meherzad Lahewala <meherzad.lahewala@gmail.com>
2020-05-31 12:59:40 -0400

    Persist userInput as part of call response

--- src/main/java/com/followapp/core/data/CallHistoryDao.java
+++ src/main/java/com/followapp/core/data/CallHistoryDao.java
@@ -1,35 +1,27 @@
 package com.followapp.core.data;
 
 
-import com.followapp.core.model.CallResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCallback;
-import org.springframework.jdbc.core.PreparedStatementSetter;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Component;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.sql.Types;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.List;
 
 @Component
 public class CallHistoryDao {
 
     private static final Logger LOG = LoggerFactory.getLogger(CallHistoryDao.class);
-    private static final String SAVE_CALL_HISTORY = "call update_call_status( ?, ?, ?, CURDATE(), ?)";
+    private static final String SAVE_CALL_USER_INPUT = "Update schedule_run SET user_response = ?, updated_datetime = ? WHERE ivr_request_id = ?";
     private static final String SAVE_CALL_DURATION = "Update schedule_run SET call_duration = ?, updated_datetime = ? WHERE ivr_request_id = ?";
     private static final String SAVE_MESSAGE_STATUS = "Update schedule_run SET status = ?, updated_datetime = ? WHERE ivr_request_id = ?";
     private static final String SCHEDULE_RUN_REQUEST_WITHSTATUS = "select sr.ivr_request_id from schedule_run sr" +
@@ -57,22 +49,13 @@ public class CallHistoryDao {
         });
     }
 
-    public void updateCallStatus(final CallResult callResult) {
-        LOG.info("Updating call status for " + callResult);
-
-        jdbcTemplate.execute(SAVE_CALL_HISTORY, new PreparedStatementCallback<Boolean>() {
-
+    public void updateCallStatus(final String requestId, String userInput) {
+        jdbcTemplate.execute(SAVE_CALL_USER_INPUT, new PreparedStatementCallback<Boolean>() {
             @Override
             public Boolean doInPreparedStatement(PreparedStatement ps) throws SQLException, DataAccessException {
-                ps.setString(1, callResult.getSid());
-                if (callResult.getCallDetails() == null) {
-                    ps.setNull(2, Types.INTEGER);
-                    ps.setNull(3, Types.INTEGER);
-                } else {
-                    ps.setInt(2, callResult.getCallDetails().getCareRecipientId());
-                    ps.setInt(3, callResult.getCallDetails().getPrescriptionDetailsId());
-                }
-                ps.setString(4, callResult.getCallStatus().toString());
+                ps.setString(1, userInput);
+                ps.setTimestamp(2, Timestamp.valueOf(LocalDateTime.now()));
+                ps.setString(3, requestId);
                 return ps.execute();
             }
         });
