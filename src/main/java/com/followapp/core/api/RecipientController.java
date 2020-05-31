9c06b14e88f7084210921da20c9d4d390838184e
sunnynagpal <sunnynagpal@outlook.com>
2020-06-01 00:51:10 +0530

    Recipient API (#24)

---
+++ src/main/java/com/followapp/core/api/RecipientController.java
@@ -0,0 +1,49 @@
+package com.followapp.core.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import com.followapp.core.data.CallingGroupRepository;
+import com.followapp.core.data.RecipientRepository;
+import com.followapp.core.model.Recipient;
+import com.followapp.core.model.CallingGroup;
+import com.followapp.core.model.CallingGroupUpdateMembershipRequest;
+
+import java.util.Optional;
+
+
+@Controller
+@RequestMapping(path="/recipients")
+public class RecipientController {
+    @Autowired
+    private RecipientRepository recipientRepository;
+
+    @PostMapping(path = "/create-recipient")
+    public @ResponseBody String createNewGroup(@RequestBody Recipient recipient) {        	        	
+        recipientRepository.createRecipient(recipient);
+        return String.format("Created New Recipient %s", recipient);
+    }
+    
+    @PostMapping(path = "/update-group-membership")
+    public @ResponseBody String updateGroupMembership(@RequestBody CallingGroupUpdateMembershipRequest callingGroupUpdateMembershipRequest) {        	        	
+        recipientRepository.updateGroupMembership(callingGroupUpdateMembershipRequest);
+        return String.format("Updated Group Membership %s", callingGroupUpdateMembershipRequest);
+    }    
+
+    @GetMapping(("/get-all-recipients"))
+    public @ResponseBody Iterable<Recipient> getAllGroups() {    	    	    
+        return recipientRepository.getAllRecipients();    	    	
+    }
+
+    @GetMapping("/{id}")
+    public @ResponseBody Optional<Recipient> getCallingGroup(@PathVariable Integer id) {        
+    	return Optional.ofNullable(recipientRepository.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public @ResponseBody String deleteCallingGroup(@PathVariable Integer id) {
+        recipientRepository.deleteById(id);
+        return "Deleted " + id;
+    }
+}
